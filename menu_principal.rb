897e7cf8677fbfe0e64f7e9f18ef02ae9ab04c89
MaritzaFSilva <maritzafer98@gmail.com>
2021-03-23 10:14:55 -0300

    Add Character Class

--- menu_principal.rb
+++ menu_principal.rb
@@ -1,10 +1,51 @@
+require_relative './tipos_raca'
+require_relative './personagem'
+require_relative './classe'
+
+$classes_salvas = []
+
+def load_static_classes
+    classses = [] 
+    classses.append Classe.new 'Construtor', 'Construir casas', {forca: 1, inteligencia: 1, vida: -2, ataque: 0, defesa: 0}
+    classses.append Classe.new 'Ferreiro', 'Construir espadas e armaduras', {forca: 2, inteligencia: 0, vida: 0, ataque: 1, defesa: 0}
+    classses.append Classe.new 'Curandeiro', 'Curar outras unidades', {forca: 3, inteligencia: 3, vida: 0, ataque: -1, defesa: 0}
+    classses.append Classe.new 'Guerreiro', 'Atacar e defender', {forca: 2, inteligencia: 1, vida: 0, ataque: 3, defesa: 0}
+    classses
+end
+
+
+# Cria uma nova classe 
+def create_class
+    puts 'Nome da classe a ser criada:'
+    nome_classe = gets.strip.to_s
+    puts 'Habilidades:'
+    habilidade = gets.strip.to_s
+
+    puts '---- Informe os modificadores ----'
+    puts "Forca:"
+    forca_classe = gets.to_i
+    puts "Vida:"
+    vida_classe = gets.to_i
+    puts "Ataque:"
+    ataque_classe = gets.to_i
+    puts "Inteligencia:"
+    inteligencia_classe = gets.to_i
+    puts "Defesa:"
+    defesa_classe = gets.to_i
+
+    modificadores = {forca: forca_classe, inteligencia: inteligencia_classe, vida: vida_classe, ataque: ataque_classe, defesa: defesa_classe}
+    classe = Classe.new nome_classe, habilidade, modificadores
+    $classes_salvas << classe
+    puts "Classe #{classe.to_s} salva"
+end
+
 
 
 
 #inicializa em 8 pra que seja verdadeira a condição do while
 op = 8
 
-
+$classes_salvas = load_static_classes
 while op != 0
     
     puts "\n\n----------------------- MENU -----------------------"
@@ -19,7 +60,7 @@ while op != 0
       
     elsif op == 2
         puts " > Cadastrar Classe"
-       
+        create_class
     elsif op == 3
         puts " > Treinar"
        
