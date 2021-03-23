e0e8b79ed3057a9fc2e6c6e0c1ae6c96516dea2b
MaritzaFSilva <maritzafer98@gmail.com>
2021-03-23 10:09:47 -0300

    Create Classe

---
+++ classe.rb
@@ -0,0 +1,12 @@
+class Classe
+    attr_accessor :nome_classe, :habilidade, :modificadores, :classes
+    
+    def initialize nome_classe, habilidade, modificadores
+        @nome_classe = nome_classe
+        @habilidade = habilidade
+        @modificadores = modificadores
+    end
+    def to_s
+        puts nome_classe
+    end
+end
